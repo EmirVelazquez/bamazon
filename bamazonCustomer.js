74b4c69115ff9a5c710a3826460615c76bef8475
EmirVelazquez <emirleovelazquez@gmail.com>
2019-11-30 19:15:28 -0600

    added logic to ask user what product and how many to purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,6 +20,7 @@ var connection = mysql.createConnection({
 // Connect to mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
+    console.log("Welcome shopper, These are the current products available for sale!");
     //Runs the display all products for same function
     displayAll();
 });
@@ -29,7 +30,6 @@ connection.connect(function (err) {
 function displayAll() {
     // Query database for all products for sale
     connection.query("SELECT * FROM products", function (err, results) {
-        console.log("Welcome shopper, These are the current products available for sale!");
         if (err) throw err;
         let productArr = [];
         for (var i = 0; i < results.length; i++) {
@@ -38,6 +38,7 @@ function displayAll() {
             productArr.push(new Product(results[i].item_id, results[i].product_name, results[i].price));
         }
         console.table(productArr);
+        whichProduct();
     });
 };
 
@@ -46,4 +47,47 @@ function Product(item_id, product_name, price) {
     this.item_id = item_id;
     this.product_name = product_name;
     this.price = price;
+}
+
+// Function to ask user what product they would like to buy 
+function whichProduct() {
+    // Query database for all products being sold
+    connection.query("SELECT *FROM products", function (err, results) {
+        // Prompt user what item they would like to buy
+        inquirer.prompt([
+            {
+                name: "buyProduct",
+                type: "list",
+                message: "Which item_id do you want to purchase?",
+                choices: function () {
+                    var choiceId = [];
+                    for (var i = 0; i < results.length; i++) {
+                        choiceId.push(results[i].item_id);
+                    }
+                    return choiceId;
+                }
+            },
+            {
+                name: "quantityProduct",
+                type: "number",
+                message: "How many would you like to purchase?"
+            }
+        ]).then(function (answer) {
+            var chosenProduct;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].item_id === answer.buyProduct) {
+                    chosenProduct = results[i];
+                }
+            }
+            console.log(chosenProduct.stock_quantity);
+            console.log(chosenProduct.stock_quantity - answer.quantityProduct);
+            // Determine if there are still enough products in stock
+            if (chosenProduct.stock_quantity > parseInt(answer.quantityProduct)) {
+                console.log("Enough products in stock for purchase!");
+            }
+            else {
+                console.log("Not enough product stock to fulfill your request, sorry!");
+            }
+        });
+    });
 }
